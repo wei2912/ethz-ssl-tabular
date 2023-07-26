d0b8c84404e050fc4906def18ef73f0d1ac25dee
Ng Wei En <weien1292@gmail.com>
2023-07-26 16:17:54 +0200

    self_training: fix typo

--- src/models/self_training.py
+++ src/models/self_training.py
@@ -109,7 +109,7 @@ class SelfTrainingModel_CurriculumSingleIterate(SemiSLModel):
                 y_pl, ids, axis=0
             )
         else:
-            ids = list(range(len(X_train_pl)))
+            ids = list(range(len(X_train_ul)))
             X_train_pl, y_train_pl = X_train_ul, y_pl
 
         X, y = np.concatenate((X_train, X_train_pl)), np.concatenate(
@@ -188,7 +188,7 @@ class SelfTrainingModel_Curriculum(SemiSLModel):
                     y_pl, ids, axis=0
                 )
             else:
-                ids = list(range(len(X_train_pl)))
+                ids = list(range(len(X_train_ul)))
                 X_train_pl, y_train_pl = X_train_ul, y_pl
 
             X, y = np.concatenate((X_train, X_train_pl)), np.concatenate(
